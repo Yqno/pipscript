74724fd3e3a22b79e2b32c7a3859d68fd2d27ced
Y Q N O <79216939+Yqno@users.noreply.github.com>
2024-08-29 14:41:28 +0200

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-apt-get install python3-pip -y 
-pip install colorama instagrapi pillow numpy scapy python-nmap bs4 tkinter colorama pynput
+apt-get install python3-dev python3-virtualenv python3 python3-pip
+virtualenv myenv
+source myenv/bin/activate
+pip install colorama instagrapi pillow numpy scapy python-nmap bs4 tkinter colorama pynput holehe discord httpx requests
